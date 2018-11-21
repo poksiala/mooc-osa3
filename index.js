7e9a34d5f75696de4b73447372ab3809f3dc5b90
Panu Oksiala <panu@oksiala.fi>
2018-11-21 15:24:51 +0200

    3-01

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('hello world')
+const express = require('express')
+const app = express()
+const PORT = 3001
+
+const persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+      },
+      {
+        "name": "Martti Tienari",
+        "number": "040-123456",
+        "id": 2
+      },
+      {
+        "name": "Arto Järvinen",
+        "number": "040-123456",
+        "id": 3
+      },
+      {
+        "name": "Lea Kutvonen",
+        "number": "040-123456",
+        "id": 4
+      },  
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+
+
+app.listen(PORT, () => console.log(`sevu portissa ${PORT}`))
